41c9a061c7fc0a793431db35bb43779bb18d4730
vincent <vincentk222@hotmail.com>
2023-02-19 10:25:28 +0100

    update

--- install.sh
+++ install.sh
@@ -27,8 +27,7 @@ sudo apt-get update
 sudo apt remove -y docker docker-engine docker.io containerd runc
 
 # Install Docker and Docker Compose
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io auditd
 # Download the latest version of Docker Compose
 LATEST_VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
 sudo curl -L "https://github.com/docker/compose/releases/download/${LATEST_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
